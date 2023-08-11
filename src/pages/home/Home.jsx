38a5dc56ff05fa8848abd355d991c7f14918e304
nahid-dev <nahidv232@gmail.com>
2023-08-11 12:10:27 +0600

    routing done

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HomeBanner from "../../components/homeBanner/HomeBanner";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Home page</h1>
+      <HomeBanner></HomeBanner>
+    </div>
+  );
+};
+
+export default Home;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,18 @@
-const { createBrowserRouter } = require("react-router-dom");
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/home/Home";
 
-const router = createBrowserRouter([{}]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
 
 export default router;
